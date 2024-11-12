fcfd06cc95ad6097fe89a3056ae26d8083c7ba85
karthikn25 <mkarthikeyan179@gmail.com>
2024-11-12 09:29:17 +0530

    first commit

--- src/Component/CategoryList/CategoryList.js
+++ src/Component/CategoryList/CategoryList.js
@@ -1,90 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "./CategoryList.css";
-// import Base from "../../Base/Base";
-// import { useNavigate, useParams } from "react-router-dom";
-// import { URL } from "../Server";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getAllFoods } from "../../Redux/Actions/foodAction";
-
-// export default function CategoryList() {
-//   const [loading,setLoading]=useState(false);
-//   const dispatch = useDispatch();
-//   const {foods,error}=useSelector((state)=>state.food);
-//   const [expandedItemId, setExpandedItemId] = useState(null);
-//   const [food, setFood] = useState([]);
-//   const navigate = useNavigate();
-//   const { id, name } = useParams();
-
-//   useEffect(() => {
-//     const fetchFood = async()=>{
-//       try {
-//         setLoading(true);
-//         dispatch(getAllFoods(id))
-//       } catch (error) {
-//         console.log(`Error occured to fetch foods ${error}`);
-//       }finally{
-//         setLoading(false)
-//       }
-//     }
-//     fetchFood();
-//   }, []);
-
-
-
-//   const toggle = (itemId) => {
-//     setExpandedItemId(itemId === expandedItemId ? null : itemId);
-//   };
-
-//   return (
-//     <div id="category-list">
-//       <Base>
-//         <div id="category-list-container">
-//           <div id="category-list-head">
-//             <h5 style={{fontFamily:"cursive"}}>{name}</h5>
-//             <i className="bx bx-home" onClick={() => navigate("/")} />
-//           </div>
-
-//           <div id="category-list-item-container">
-//             {food.map((d) => (
-//               <div
-//                 key={d.id} // Ensure each item has a unique key
-//                 id="category-list-item-box"
-//                 onClick={() => toggle(d._id)}
-//                 style={{ height: expandedItemId === d._id || !d.status ? "max-content" : "50px" ,opacity: d.status ? 1 : 0.5, }}
-//               >
-//                 <div id="category-lists">
-//                   <div id="show-container">
-
-//                   <div id="f-detail">
-//                   <div id="f-details">
-//                   <img src={d.photo} alt={d.foodName}/>
-//                   <h6 style={{fontFamily:"cursive"}}>{d.foodName}</h6>
-//                   </div>
-                  
-                  
-//                   <p id="food-price" style={{fontFamily:"cursive"}}>Rs: {d.price}</p></div>
-                    
-//                     <div id="f-status">{!d.status && <p style={{ color: 'red' ,fontFamily:"cursive"}}>Out of Stock</p>}
-//                     </div>
-//                     <p
-//                     id="category-detail"
-//                     style={{ display: expandedItemId === d._id ? "block" : "none" ,fontFamily:"cursive"}}
-//                   >
-//                     {d.details}
-//                   </p>
-//                   </div>
-                 
-//                 </div>
-                
-//               </div>
-              
-//             ))}
-//           </div>
-//         </div>
-//       </Base>
-//     </div>
-//   );
-// }
 import React, { useEffect, useState } from "react";
 import "./CategoryList.css";
 import Base from "../../Base/Base";
@@ -165,7 +78,7 @@ export default function CategoryList() {
 
                       <div className="f-status">
                         {!d.status && (
-                          <p style={{ color: "red", fontFamily: "cursive" }}>Out of Stock</p>
+                          <p style={{ color: "red"}}>Out of Stock</p>
                         )}
                       </div>
                    
@@ -175,7 +88,7 @@ export default function CategoryList() {
                         className="category-detail"
                         style={{
                           display: expandedItemId === d._id ? "block" : "none",
-                          fontFamily: "cursive",
+                         
                         }}
                       >
                         {d.details}
